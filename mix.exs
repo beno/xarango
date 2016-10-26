d6b5c03417c8a88440183f5573cb5608d67d071a
Michel Benevento <michelbenevento@yahoo.com>
2016-10-26 16:46:05 +0200

    update package spec

--- mix.exs
+++ mix.exs
@@ -2,35 +2,44 @@ defmodule Xarango.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :xarango,
+    [
+     app: :xarango,
      version: "0.1.0",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(),
+     description: description(),
+     package: package()
+    ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger, :httpotion, :poison]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
+  defp description do
+    """
+    Client library for ArangoDB.
+    """
+  end
+  
+  defp package do
+    [
+     name: :xarango,
+     files: ["lib", "mix.exs", "README*", "LICENSE"],
+     maintainers: ["Michel Benevento"],
+     licenses: ["Apache 2.0"],
+     links: %{"GitHub" => "https://github.com/beno/xarango"} 
+    ]
+  end
+
   defp deps do
     [
       {:httpotion, "> 0.0.0"},
       {:poison, "> 0.0.0"},
-      {:faker, "> 0.0.0", only: :test}
+      {:faker, "> 0.0.0", only: :test},
+      {:ex_doc, ">= 0.0.0", only: :dev}
     ]
   end
 end
