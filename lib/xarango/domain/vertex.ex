8cfc5f834f03fb89fa79caa39ad8777f9dfa46f8
Michel Benevento <michelbenevento@yahoo.com>
2016-10-29 21:58:54 +0200

    add domain vertex

--- lib/xarango/domain/vertex.ex
+++ lib/xarango/domain/vertex.ex
@@ -1,6 +1,7 @@
 defmodule Xarango.Domain.Vertex do
 
   alias Xarango.Vertex
+  alias Xarango.SimpleQuery
   
   defmacro __using__(_options) do
     quote do
@@ -17,13 +18,48 @@ defmodule Xarango.Domain.Vertex do
     quote do
       defp database, do: %Xarango.Database{name: unquote(db)}
       defp graph, do: %Xarango.Graph{name: unquote(gr)}
-      defp vertex_collection, do: %Xarango.VertexCollection{collection: unquote(coll)}
+      defp collection, do: %Xarango.VertexCollection{collection: unquote(coll)}
       def create(data, options\\[]) do
         database = Xarango.Database.ensure(database)
-        vc = Xarango.VertexCollection.ensure(vertex_collection, graph, database)
+        vc = Xarango.VertexCollection.ensure(collection, graph, database)
         vertex = Vertex.create(%Vertex{_data: data}, vc, graph, database) |> Vertex.vertex(vc, graph, database)
         struct(__MODULE__, vertex: vertex)
       end
+      def one(params) do
+        document = SimpleQuery.first_example(%SimpleQuery{example: params, collection: collection.collection}, database)
+        struct(__MODULE__, vertex: to_vertex(document))
+      end
+      def list(params) do
+        SimpleQuery.by_example(%SimpleQuery{example: params, collection: collection.collection}, database)
+        |> Enum.map(&struct(__MODULE__, vertex: to_vertex(&1)))
+      end
+      def replace(params, data) do
+        vertex = %{ one(params).vertex | _data: data }
+          |> Vertex.replace(collection, graph, database)
+          |> Vertex.vertex(collection, graph, database)
+        struct(__MODULE__, vertex: vertex)
+      end
+      def update(params, data) do
+        vertex = %{ one(params).vertex | _data: data }
+          |> Vertex.update(collection, graph, database)
+          |> Vertex.vertex(collection, graph, database)
+        struct(__MODULE__, vertex: vertex)
+      end
+      def destroy(params) do
+        one(params).vertex
+        |> Vertex.destroy(collection, graph, database)
+      end
+      def fetch(vertex, field) do
+        value = vertex.vertex._data
+          |> Map.get(field)
+        {:ok, value}
+      end
+      defp to_vertex(document) do
+        doc = Map.from_struct(document)
+        struct(Xarango.Vertex, doc) 
+      end
+
+
     end
   end
   
--- test/domain/vertex_test.exs
+++ test/domain/vertex_test.exs
@@ -2,27 +2,103 @@ defmodule DomainVertexTest do
   use ExUnit.Case
   doctest Xarango
 
-  # setup do
-  #   on_exit fn ->
-  #     # try do Xarango.Collection.destroy(%Xarango.Collection{name: "test_collection"}) rescue _ -> nil end
-  #     # try do Xarango.Collection.destroy(%Xarango.Collection{name: "test_collection"}, _database) rescue _ -> nil end
-  #     # try do Xarango.Database.destroy(_database) rescue _ -> nil end
-  #   end
-  # end
+  setup do
+    on_exit fn ->
+      Xarango.Graph.__destroy_all()
+      Xarango.Graph.__destroy_all(_database)
+      Xarango.Collection.__destroy_all()
+      Xarango.Collection.__destroy_all(_database)
+    end
+  end
 
-  test "create graph" do
-    model = TestVertex.create(%{jabba: "dabba"})
-    assert model.vertex._data == %{jabba: "dabba"} 
+  test "create vertex" do
+    vertex = TestVertex.create(%{jabba: "dabba"})
+    assert vertex.vertex._data == %{jabba: "dabba"}
   end
   
-  test "create graph in db" do
+  test "create vertex in db" do
+    vertex = TestDbVertex.create(%{jabba: "dabba"})
+    assert vertex.vertex._data == %{jabba: "dabba"} 
+  end
+  
+  test "get one vertex" do
+    source = TestVertex.create(%{jabba: "dabba"})
+    model = TestVertex.one(%{_id: source.vertex._id})
+    assert model.vertex._data == %{jabba: "dabba"}
+  end
+  
+  test "get one model in db" do
+    source = TestDbVertex.create(%{jabba: "dabba"})
+    model = TestDbVertex.one(%{_id: source.vertex._id})
+    assert model.vertex._data == %{jabba: "dabba"}
+  end
+  
+  test "get model list" do
+    source = TestVertex.create(%{jabba: "dabba"})
+    TestVertex.create(%{dabba: "doo"})
+    models = TestVertex.list(%{jabba: "dabba"})
+    assert is_list(models)
+    assert length(models) == 1
+    assert Enum.at(models, 0).vertex._data == source.vertex._data
+  end
+  
+  test "list models in db" do
+    source = TestDbVertex.create(%{jabba: "dabba"})
+    TestDbVertex.create(%{dabba: "doo"})
+    models = TestDbVertex.list(%{jabba: "dabba"})
+    assert is_list(models)
+    assert length(models) == 1
+    assert Enum.at(models, 0).vertex._data == source.vertex._data
+  end
+  
+  
+  test "replace model" do
+    source = TestVertex.create(%{jabba: "dabba"})
+    model = TestVertex.replace(%{_id: source.vertex._id},  %{foo: "bar"})
+    assert model.vertex._data == %{foo: "bar"}
+  end
+  
+  test "replace in db" do
+    source = TestDbVertex.create(%{jabba: "dabba"})
+    model = TestDbVertex.replace(%{_id: source.vertex._id},  %{foo: "bar"})
+    assert model.vertex._data == %{foo: "bar"}
+  end
+  
+  test "update model" do
+    source = TestVertex.create(%{jabba: "dabba"})
+    model = TestVertex.update(%{_id: source.vertex._id},  %{foo: "bar"})
+    assert model.vertex._data == %{jabba: "dabba", foo: "bar"}
+  end
+  
+  test "update in db" do
+    source = TestDbVertex.create(%{jabba: "dabba"})
+    model = TestDbVertex.update(%{_id: source.vertex._id},  %{foo: "bar"})
+    assert model.vertex._data == %{jabba: "dabba", foo: "bar"}
+  end
+  
+  test "destroy model" do
+    source = TestVertex.create(%{jabba: "dabba"})
+    result = TestVertex.destroy(%{_id: source.vertex._id})
+    assert result[:removed] == true
+  end
+  
+  test "destroy in db" do
+    source = TestDbVertex.create(%{jabba: "dabba"})
+    result = TestDbVertex.destroy(%{_id: source.vertex._id})
+    assert result[:removed] == true
+  end
+  
+  test "access" do
     model = TestDbVertex.create(%{jabba: "dabba"})
-    assert model.vertex._data == %{jabba: "dabba"} 
+    assert model[:jabba] == "dabba"
   end
-  
 
+  
+  defp _database, do: %Xarango.Database{name: "test_database"}
+  # defp _vertex_collection, do: %Xarango.VertexCollection{collection: "test_collection"}
   
 end
+
 
 defmodule TestVertex do
   use Xarango.Domain.Vertex
