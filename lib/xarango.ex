5ac2b36faf9cfb2c2c33009f8934a57c18fc5fb5
Michel Benevento <michelbenevento@yahoo.com>
2016-10-31 00:02:55 +0100

    util

--- lib/xarango.ex
+++ lib/xarango.ex
@@ -1,2 +1,13 @@
 defmodule Xarango do
 end
+
+defmodule Xarango.Util do
+  
+  def name_from(module) do
+    module
+    |> Module.split
+    |> Enum.join("")
+    |> Macro.underscore
+  end
+  
+end
