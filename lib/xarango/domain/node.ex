5f4f900ef19c4fc6560a201df83d3c1034a50a42
Michel Benevento <michelbenevento@yahoo.com>
2016-11-08 00:34:49 +0100

    refactor

--- lib/xarango/domain/node.ex
+++ lib/xarango/domain/node.ex
@@ -8,6 +8,7 @@ defmodule Xarango.Domain.Node do
     database = options[:db] && Atom.to_string(options[:db]) || Xarango.Server.server.database
     graph = options[:graph]
     quote do
+      alias Xarango.Domain.Node
       defstruct vertex: %Xarango.Vertex{}
       defp _database, do: %Xarango.Database{name: unquote(database)}
       defp _graph_module do
@@ -18,49 +19,55 @@ defmodule Xarango.Domain.Node do
       end
       defp _graph(nil), do: %Xarango.Graph{name: Xarango.Util.name_from(_graph_module)}
       defp _graph(graph), do: %Xarango.Graph{name: Xarango.Util.name_from(graph)}
-      defp _collection do
-        %Xarango.VertexCollection{collection: Xarango.Util.name_from(__MODULE__)}
-      end
-      def create(data, graph\\nil) do
-        (graph || _graph_module).ensure()
-        vc = Xarango.VertexCollection.ensure(_collection, _graph(graph), _database)
-        vertex = Vertex.create(%Vertex{_data: data}, vc, _graph(graph), _database) |> Vertex.vertex(vc, _graph(graph), _database)
-        struct(__MODULE__, vertex: vertex)
-      end
-      def one(params, _gr\\nil) do
-        document = SimpleQuery.first_example(%SimpleQuery{example: params, collection: _collection.collection}, _database)
-        struct(__MODULE__, vertex: to_vertex(document))
-      end
-      def list(params, _gr\\nil) do
-        SimpleQuery.by_example(%SimpleQuery{example: params, collection: _collection.collection}, _database)
-        |> Enum.map(&struct(__MODULE__, vertex: to_vertex(&1)))
-      end
-      def replace(vertex, data, graph\\nil) do
-        vertex = %{ vertex.vertex | _data: data }
-          |> Vertex.replace(_collection, _graph(graph), _database)
-          |> Vertex.vertex(_collection, _graph(graph), _database)
-        struct(__MODULE__, vertex: vertex)
-      end
-      def update(vertex, data, graph\\nil) do
-        vertex = %{ vertex.vertex | _data: data }
-          |> Vertex.update(_collection, _graph(graph), _database)
-          |> Vertex.vertex(_collection, _graph(graph), _database)
-        struct(__MODULE__, vertex: vertex)
-      end
-      def destroy(vertex, graph\\nil) do
-        vertex.vertex
-        |> Vertex.destroy(_collection, _graph(graph), _database)
-      end
-      def fetch(vertex, field) do
-        value = vertex.vertex._data
-          |> Map.get(field)
-        {:ok, value}
-      end
-      defp to_vertex(document) do
-        doc = Map.from_struct(document)
-        struct(Xarango.Vertex, doc) 
-      end
+      defp _collection, do: %Xarango.VertexCollection{collection: Xarango.Util.name_from(__MODULE__)}
+      def create(data, options\\[]), do: Node.create(data, _collection, _graph(options[:graph]), _database) |> to_node
+      def one(params, options\\[]), do: Node.one(params, _collection, _graph(options[:graph]), _database) |> to_node
+      def list(params, options\\[]), do: Node.list(params, _collection, _graph(options[:graph]), _database) |> to_nodes
+      def replace(node, data, options\\[]), do: Node.replace(node, data, _collection, _graph(options[:graph]), _database) |> to_node
+      def update(node, data, options\\[]), do: Node.update(node, data, _collection, _graph(options[:graph]), _database) |> to_node
+      def destroy(node, options\\[]), do: Node.destroy(node, _collection, _graph(options[:graph]), _database)
+      defp to_node(vertex), do: struct(__MODULE__, vertex: vertex)
+      defp to_nodes(vertices), do: vertices |> Enum.map(&struct(__MODULE__, vertex: &1))
     end
   end
+  
+  def create(data, collection, graph, database) do
+    graph.ensure()
+    vc = Xarango.VertexCollection.ensure(collection, graph, database)
+    Vertex.create(%Vertex{_data: data}, vc, graph, database)
+    |> Vertex.vertex(vc, graph, database)
+  end
+  def one(params, collection, _graph, database) do
+    document = SimpleQuery.first_example(%SimpleQuery{example: params, collection: collection.collection}, database)
+    struct(__MODULE__, vertex: to_vertex(document))
+  end
+  def list(params, collection, _graph, database) do
+    SimpleQuery.by_example(%SimpleQuery{example: params, collection: collection.collection}, database)
+    |> Enum.map(&to_vertex(&1))
+  end
+  def replace(node, data, collection, graph, database) do
+    %{ node.vertex | _data: data }
+    |> Vertex.replace(collection, graph, database)
+    |> Vertex.vertex(collection, graph, database)
+  end
+  def update(node, data, collection, graph, database) do
+    %{ node.vertex | _data: data }
+    |> Vertex.update(collection, graph, database)
+    |> Vertex.vertex(collection, graph, database)
+  end
+  def destroy(node, collection, graph, database) do
+    node.vertex
+    |> Vertex.destroy(collection, graph, database)
+  end
+  def fetch(node, field) do
+    value = node.vertex._data
+      |> Map.get(field)
+    {:ok, value}
+  end
+  defp to_vertex(document) do
+    doc = Map.from_struct(document)
+    struct(Xarango.Vertex, doc) 
+  end
+
       
 end
