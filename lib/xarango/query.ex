96b830b17a8039dcad4ad41ff012cc12c06d88b6
M Benevento <michel@Ms-MacBook-Pro.local>
2017-06-10 02:05:56 +0200

    query building api

--- lib/xarango/query.ex
+++ lib/xarango/query.ex
@@ -3,6 +3,7 @@ defmodule Xarango.Query do
   defstruct [:query, :count, :ttl, :batchSize, :cache, :bindVars, :options]
 
   alias Xarango.Query
+  alias Xarango.AQL
   import Xarango.Client
   use Xarango.URI, prefix: "cursor"
 
@@ -10,7 +11,13 @@ defmodule Xarango.Query do
     query(%Query{query: query})
   end
 
-  def query(query, database\\nil) do
+  def query(query, database\\nil)
+  def query(%Query{query: %AQL{}} = query, database) do
+    url("", database)
+    |> post(%Query{query | query: AQL.to_aql(query.query)})
+    |> to_result
+  end
+  def query(query, database) do
     url("", database)
     |> post(query)
     |> to_result
@@ -41,6 +48,42 @@ defmodule Xarango.Query do
   defp to_explanation(data) do
     struct(Xarango.Explanation, data)
   end
+  
+  def from(collection) do
+    %Xarango.Query{query: AQL.from(collection)}
+  end
+
+  def filter(query, nil), do: query
+  def filter(query, filter) when is_map(filter) do
+    where(query, Map.to_list(filter))
+  end
+  def filter(query, filter) do
+    %Xarango.Query{query | query: AQL.filter(query.query, filter) }
+  end
+  defdelegate where(query, filters), to: __MODULE__, as: :filter
+  
+  def sort(query, nil), do: query
+  def sort(query, sort) do
+    %Xarango.Query{query | query: AQL.sort(query.query, sort) }
+  end
+  
+  def limit(query, nil), do: query
+  def limit(query, limit) do
+    %Xarango.Query{query | query: AQL.limit(query.query, limit) }
+  end
+  
+  def paginate(query, nil), do: query
+  def paginate(query, page_length) do
+    %Xarango.Query{query | batchSize: page_length }
+  end
+  
+  def build(collection, filter, options) do
+    from(collection)
+    |> filter(filter)
+    |> sort(options[:sort])
+    |> limit(options[:limit])
+    |> paginate(options[:per_page])
+  end
 
 end
 
@@ -55,3 +98,4 @@ defmodule Xarango.Explanation do
   defstruct [:plans, :warnings, :stats, :cacheable, :error, :code]
 
 end
+
--- test/query_test.exs
+++ test/query_test.exs
@@ -20,6 +20,17 @@ defmodule QueryTest do
     assert result.error == false
     assert length(result.result) == length
   end
+  
+  test "query builder" do
+    coll = %{name: "products"}
+    q = Query.from(coll)
+    |> Query.where([foo: "bar", size: 4])
+    |> Query.where(%{age: 10})
+    |> Query.limit(4)
+    |> Query.paginate(20)
+    assert Xarango.AQL.to_aql(q.query) == "FOR r IN products FILTER r.foo == \"bar\" FILTER r.size == 4 FILTER r.age == 10 LIMIT 4 RETURN r"
+    assert q.batchSize == 20
+  end
 
   test "next" do
     length = 5
@@ -38,7 +49,15 @@ defmodule QueryTest do
     explanation = Query.explain(%Query{query: q})
     assert explanation.error == false
   end
-
+  
+  test "build" do
+    collection = %{name: "things"}
+    filter = [name: "foo"]
+    options = [per_page: 20, sort: :name]
+    q = Query.build(collection, filter, options)
+    assert Xarango.AQL.to_aql(q.query) == "FOR r IN things FILTER r.name == \"foo\" SORT r.name DESC RETURN r"
+    assert q.batchSize == 20
+  end
 
   defp _documents(count) do
     coll = Xarango.Collection.create(collection_())
