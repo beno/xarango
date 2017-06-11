527bd0352cc5fee2d14024c6bef0970c533a6fd1
M Benevento <michel@Ms-MacBook-Pro.local>
2017-06-12 01:38:59 +0200

    Add ensure, refactor graph

--- lib/xarango/domain/graph.ex
+++ lib/xarango/domain/graph.ex
@@ -21,8 +21,6 @@ defmodule Xarango.Domain.Graph do
       Module.register_attribute __MODULE__, :relationships, accumulate: true
       def _database, do: %Database{name: unquote(db)}
       defp _graph, do: %Graph{name: unquote(gr) || Xarango.Util.name_from(__MODULE__) }
-      defp _relationships, do: []
-      defoverridable [_relationships: 0]
       def create, do: ensure()
       def ensure do
         Database.ensure(_database())
@@ -32,27 +30,43 @@ defmodule Xarango.Domain.Graph do
       end
       def destroy, do: Graph.destroy(_graph().graph, _database())
       def add(from, relationship, to, data\\nil), do: add(from, relationship, to, data, _graph(), _database())
+      def ensure(from, relationship, to, data\\nil), do: ensure(from, relationship, to, data, _graph(), _database())
       def remove(from, relationship, to), do: remove(from, relationship, to, _graph(), _database())
       def get(from, relationship, to), do: get(from, relationship, to, _database())
       def traverse(start, options\\[]), do: traverse(start, options, _graph(), _database())
+      @before_compile Xarango.Domain.Graph
     end
   end
 
   defmacro relationship(from, relationship, to) do
     {relationship, from, to} = {Atom.to_string(relationship), Macro.expand(from, __CALLER__), Macro.expand(to, __CALLER__)}
-    add_method = "add_#{relationship}" |> String.to_atom
-    remove_method = "remove_#{relationship}" |> String.to_atom
-    inbound_method = "#{relationship}_#{Util.short_name_from(to)}" |> String.to_atom
-    outbound_method = "#{Util.short_name_from(from)}_#{relationship}" |> String.to_atom
     quote do
-      @relationships %{from: unquote(from), to: unquote(to), name: unquote(relationship)}
-      defp _relationships, do: @relationships
-      defoverridable [_relationships: 0]
-      def unquote(add_method)(%unquote(from){} = from, %unquote(to){} = to), do: add(from, unquote(relationship), to, nil)
-      def unquote(add_method)(%unquote(from){} = from, %unquote(to){} = to, data), do:  add(from, unquote(relationship), to, data)
-      def unquote(remove_method)(%unquote(from){} = from, %unquote(to){} = to), do: remove(from, unquote(relationship), to)
-      def unquote(inbound_method)(%unquote(from){} = from), do: get(from, unquote(relationship), unquote(to))
-      def unquote(outbound_method)(%unquote(to){} = to), do: get(unquote(from), unquote(relationship), to)
+      relationship = %{from: unquote(from), to: unquote(to), name: unquote(relationship)}
+      unless Enum.member?(@relationships, relationship), do: @relationships relationship
+    end
+  end
+  
+  defmacro __before_compile__(env) do
+    relationships = Module.get_attribute(env.module, :relationships)
+    methods = Enum.map relationships, fn %{from: from, to: to, name: relationship} ->
+      add_method = "add_#{relationship}" |> String.to_atom
+      ensure_method = "ensure_#{relationship}" |> String.to_atom
+      remove_method = "remove_#{relationship}" |> String.to_atom
+      inbound_method = "#{relationship}_#{Util.short_name_from(to)}" |> String.to_atom
+      outbound_method = "#{Util.short_name_from(from)}_#{relationship}" |> String.to_atom
+      quote do
+        def unquote(add_method)(%unquote(from){} = from, %unquote(to){} = to), do: unquote(add_method)(from, to, nil)
+        def unquote(add_method)(%unquote(from){} = from, %unquote(to){} = to, data), do:  add(from, unquote(relationship), to, data)
+        def unquote(ensure_method)(%unquote(from){} = from, %unquote(to){} = to), do: unquote(ensure_method)(from, to, nil)
+        def unquote(ensure_method)(%unquote(from){} = from, %unquote(to){} = to, data), do: ensure(from, unquote(relationship), to, data)
+        def unquote(remove_method)(%unquote(from){} = from, %unquote(to){} = to), do: remove(from, unquote(relationship), to)
+        def unquote(inbound_method)(%unquote(from){} = from), do: get(from, unquote(relationship), unquote(to))
+        def unquote(outbound_method)(%unquote(to){} = to), do: get(unquote(from), unquote(relationship), to)
+      end
+    end
+    quote do
+      def _relationships, do: @relationships
+      unquote(methods)
     end
   end
 
@@ -67,6 +81,13 @@ defmodule Xarango.Domain.Graph do
     Edge.create(edge, edge_collection, graph, database)
   end
 
+  def ensure(from, relationship, to, data, graph, database) do
+    case get(from, relationship, to, database) do
+      [] -> add(from, relationship, to, data, graph, database)
+      [edge] -> edge
+    end
+  end
+  
   def remove(from_node, relationship, to_node, graph, database) when is_atom(relationship) do
     remove(from_node, Atom.to_string(relationship), to_node, graph, database)
   end
--- test/domain/graph_test.exs
+++ test/domain/graph_test.exs
@@ -48,6 +48,17 @@ defmodule DomainGraphTest do
     assert length(alice_likes) == 2
     assert length(alice_liked_by) == 1
   end
+  
+  test "ensure relationships" do
+    TestGraph.ensure
+    alice = Person.create(%{name: "Alice"})
+    bob = Person.create(%{name: "Bob"})
+    TestGraph.ensure_likes(bob, alice)
+    TestGraph.ensure_likes(bob, alice)
+    alice_likes = TestGraph.person_likes(alice)
+    assert length(alice_likes) == 1
+  end
+
 
   test "list db relationships" do
     TestDbGraph.ensure
