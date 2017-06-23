c9c2e2e6122f4452f237682f76602ecc8129e777
beno <michel@Ms-MacBook-Pro.local>
2017-06-24 01:05:20 +0200

    Use AQL

--- lib/xarango/query.ex
+++ lib/xarango/query.ex
@@ -12,10 +12,11 @@ defmodule Xarango.Query do
   end
 
   def query(query, database\\nil)
+  def query(%AQL{} = query, database) do
+    query(%Query{query: query}, database)
+  end
   def query(%Query{query: %AQL{}} = query, database) do
-    url("", database)
-    |> post(%Query{query | query: AQL.to_aql(query.query)})
-    |> to_result
+    query(%Query{query | query: AQL.to_aql(query.query)}, database)
   end
   def query(query, database) do
     url("", database)
@@ -48,7 +49,7 @@ defmodule Xarango.Query do
   defp to_explanation(data) do
     struct(Xarango.Explanation, data)
   end
-  
+
   def from(collection) do
     %Xarango.Query{query: AQL.from(collection)}
   end
@@ -61,17 +62,21 @@ defmodule Xarango.Query do
     %Xarango.Query{query | query: AQL.filter(query.query, filter) }
   end
   defdelegate where(query, filters), to: __MODULE__, as: :filter
-  
+
   def sort(query, nil), do: query
-  def sort(query, sort) do
-    %Xarango.Query{query | query: AQL.sort(query.query, sort) }
+  def sort(query, sort), do: sort(query, sort, :asc)
+  def sort(query, nil, nil), do: query
+  def sort(query, sort, nil), do: sort(query, sort, :asc)
+  def sort(query, sort, dir) do
+    %Xarango.Query{query | query: AQL.sort(query.query, sort, dir) }
   end
-  
+
+
   def limit(query, nil), do: query
   def limit(query, limit) do
     %Xarango.Query{query | query: AQL.limit(query.query, limit) }
   end
-  
+
   def paginate(query, nil), do: query
   def paginate(query, page_length, page\\nil)
   def paginate(query, page_length, nil) do
@@ -81,11 +86,15 @@ defmodule Xarango.Query do
     %Xarango.Query{query | query: AQL.limit(query.query, page_length, page_length*(page-1)) }
   end
 
-  
+  def search(query, field, value) do
+    %Xarango.Query{query | query: AQL.fulltext(query.query, field, value) }
+  end
+
+
   def build(collection, filter, options) do
     from(collection)
     |> filter(filter)
-    |> sort(options[:sort])
+    |> sort(options[:sort], options[:dir])
     |> limit(options[:limit])
     |> paginate(options[:per_page], options[:page])
   end
@@ -95,6 +104,23 @@ end
 defmodule Xarango.QueryResult do
 
   defstruct [:id, :hasMore, :extra, :error, :result, :code, :count, :cached]
+
+  def to_vertex(query_result) do
+    query_result.result
+    |> Xarango.Vertex.to_vertex
+  end
+
+  def to_edge(query_result) do
+    query_result.result
+    |> Enum.reduce([], fn [_v, e], edges ->
+      case e do
+        nil -> edges
+        edge -> edges ++ [edge]
+      end
+    end)
+    |> Xarango.Edge.to_edge
+  end
+
 
 end
 
